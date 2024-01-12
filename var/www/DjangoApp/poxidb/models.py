c5f3d3bb8fba4df720324c23c5476d6ed0eb999b
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2024-01-12 12:00:19 -0400

    adding models

--- var/www/DjangoApp/poxidb/models.py
+++ var/www/DjangoApp/poxidb/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Proteome(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=1000)
+    protein = models.CharField(max_length=10000)
+    organism = models.CharField(max_length=100)
+    antigen = models.CharField(max_length=50)
+    antigen_score = models.FloatField()
+
+    def __str__(self):
+        return f"{self.description}, {self.protein}, {self.antigen_score}, {self.antigen}"
+    
+
+class Epitope(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=1000)
+    sequence = models.CharField(max_length=50)
+    epitope_score = models.FloatField()
+    organism = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.description}, {self.sequence}, {self.epitope_score}"
