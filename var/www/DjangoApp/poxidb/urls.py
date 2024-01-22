e833816ed8034ef5390a33fa5ee8252c29b23d5e
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2024-01-22 12:22:25 -0400

    added all views

--- var/www/DjangoApp/poxidb/urls.py
+++ var/www/DjangoApp/poxidb/urls.py
@@ -5,6 +5,13 @@ from . import views
 app_name = "poxidb"
 
 urlpatterns = [
-    path("poxidb/", views.poxidb, name = "poxidb")
-    
+    path("poxidb/", views.poxidb, name = "poxidb"),
+    path("visualize_proteome/<str:organism>", views.visualize_proteome, name = "visualize_proteome"),
+    path("download_proteome/<str:organism>", views.download_proteome, name = "download_proteome"),
+    path("visualize_antigens/<str:organism>", views.visualize_antigens, name = "visualize_antigens"),
+    path("download_antigens/<str:organism>", views.download_proteome, name = "download_antigens"),
+    path("visualize_individual_epitopes/<path:description>", views.visualize_individual_epitopes, name = "visualize_individual_epitopes"),
+    path("download_individual_epitopes/<path:description>", views.download_individual_epitopes, name = "download_individual_epitopes"),
+    path("visualize_epitopes/<str:organism>", views.visualize_epitopes, name = "visualize_epitopes"),
+    path("download_epitopes/<str:organism>", views.download_epitopes, name = "download_epitopes")
 ]
--- var/www/DjangoApp/poxidb/views.py
+++ var/www/DjangoApp/poxidb/views.py
@@ -35,3 +35,141 @@ def poxidb(request):
 
     # Include organism_data in the dictionary
     return render(request, "poxidb/poxidb.html", {'organism_data': organism_data})
+
+
+def visualize_proteome(request, organism):
+    proteome = models.Proteome.objects.filter(organism=organism)
+
+    return render(request, "poxidb/visualize_proteome.html",
+                  context={'organism':organism,
+                   'proteome':proteome,
+                   'n_proteins':len(proteome)})
+
+def download_proteome(request, organism):
+    proteome = models.Proteome.objects.filter(organism=organism)
+    proteome_data = list(proteome.values())
+
+    if proteome_data:
+        response = HttpResponse(content_type='text/plain')
+        filename = f"{organism}_proteome.fasta"
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        for entry in proteome_data:
+            description = entry['description']
+            protein = entry['protein']
+
+            response.write(f'>{description}\n')
+
+            response.write(f'{protein}\n')
+
+        return response
+    else:
+        return HttpResponse("No data available.")
+
+def visualize_antigens(request, organism):
+    antigens = models.Proteome.objects.filter(organism = organism,
+                                               antigen_score__gt=0.5)
+    antigens_data = list(antigens.values())
+    for item in antigens_data:
+        epitopes = models.Epitope.objects.filter(description = item['description'])
+        item['epitope_count'] = len(epitopes)
+
+   
+    return render(request, "poxidb/visualize_antigens.html",
+                  context={'organism':organism,
+                           'antigens':antigens_data,
+                           'n_antigens':len(antigens)})
+
+def download_antigens(request, organism):
+    antigens = models.Proteome.objects.filter(organism=organism,
+                                               antigen_score__gt=0.5)
+    antigens_data = list(antigens.values())
+
+    if antigens_data:
+        response = HttpResponse(content_type='text/plain')
+        filename = f"{organism}_antigens.fasta"
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        for entry in antigens_data:
+            description = entry['description']
+            protein = entry['protein']
+
+            response.write(f'>{description}\n')
+            response.write(f'{protein}\n')
+
+        return response
+    else:
+        return HttpResponse("No data available.")
+
+def visualize_individual_epitopes(request, description):
+
+    epitope_query = models.Epitope.objects.filter(description=description)
+    epitopes_data = list(epitope_query.values())
+
+    antigen_query = models.Proteome.objects.filter(description=description)
+    antigen = list(antigen_query.values())
+
+    for item in antigen:
+        protein = item['protein']
+
+    for item in epitopes_data:
+        
+        item['start'] = protein.find(item['sequence'])+1
+        item['end'] = protein.find(item['sequence'])+1+len(item['sequence'])-1
+
+    return render(request, "poxidb/visualize_individual_epitopes.html",
+                  context={"epitopes":epitopes_data,
+                           "antigen":antigen})
+
+def download_individual_epitopes(request, description):
+    epitopes = models.Epitope.objects.filter(description=description)
+    epitopes_data = list(epitopes.values())
+
+    if epitopes_data:
+        response = HttpResponse(content_type='text/plain')
+        filename = f"{description}_epitopes.fasta"
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        description = ''
+        for entry in epitopes_data:
+            if entry['description'] != description:
+                description = entry['description']
+                response.write(f'\n>{description}\n')
+
+            sequence = entry['sequence']
+            response.write(f'{sequence}\t')
+
+        return response
+    else:
+        return HttpResponse("No data available.")
+
+def visualize_epitopes(request, organism):
+    epitopes = models.Epitope.objects.filter(organism = organism)
+
+    return render(request, "poxidb/visualize_epitopes.html",
+                  context = {'epitopes':epitopes,
+                             'organism':organism,
+                             'n_epitopes':len(epitopes)})
+
+def download_epitopes(request, organism):
+    epitopes = models.Epitope.objects.filter(organism=organism)
+    epitopes_data = list(epitopes.values())
+
+    if epitopes_data:
+        response = HttpResponse(content_type='text/plain')
+        filename = f"{organism}_epitopes.fasta"
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        description = ''
+        for entry in epitopes_data:
+            if entry['description'] != description:
+                description = entry['description']
+                response.write(f'\n>{description}\n')
+
+            sequence = entry['sequence']
+
+            response.write(f'{sequence}\t')
+
+        return response
+    else:
+        return HttpResponse("No data available.")
